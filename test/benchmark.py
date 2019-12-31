52bc872cc9e4a9688aaae0e99d8b56e015b47b88
Andreas Wuerl <andi@tryb.de>
2019-12-31 11:45:00 +0100

    added basic benchmark

---
+++ test/benchmark.py
@@ -0,0 +1,69 @@
+import time
+
+from functional import Stream
+
+
+class Timer():
+    def __init__(self, name=''):
+        self.name = name
+        self.start = time.time()
+
+    @property
+    def elapsed(self):
+        return time.time() - self.start
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.checkpoint('done')
+        pass
+
+    def checkpoint(self, name=''):
+        print('{timer}: {checkpoint} in {elapsed} seconds'.format(
+            timer=self.name,
+            checkpoint=name,
+            elapsed=self.elapsed,
+        ).strip())
+
+
+if __name__ == '__main__':
+    iterations = 1000000
+
+    lambda1 = lambda x: x * 2
+    lambda2 = lambda x: x * x
+
+    with Timer('Stream'):
+        result = Stream(range(iterations)) \
+            .map(lambda1) \
+            .map(lambda2) \
+            .as_tuple()
+        assert len(result) == iterations
+
+    with Timer('List'):
+        result = range(iterations)
+        result = [x * 2 for x in result]
+        result = [x * x for x in result]
+        result = tuple(result)
+        assert len(result) == iterations
+
+    with Timer('List Labmda'):
+        result = range(iterations)
+        result = [lambda1(x) for x in result]
+        result = [lambda2(x) for x in result]
+        result = tuple(result)
+        assert len(result) == iterations
+
+    with Timer('Generator'):
+        result = range(iterations)
+        result = (x * 2 for x in result)
+        result = (x * x for x in result)
+        result = tuple(result)
+        assert len(result) == iterations
+
+    with Timer('Generator Lambda'):
+        result = range(iterations)
+        result = (lambda1(x) for x in result)
+        result = (lambda2(x) for x in result)
+        result = tuple(result)
+        assert len(result) == iterations
